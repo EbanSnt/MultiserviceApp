8758b55acf36b22e6df2ac05733c7c9a7c262514
Esteban Santillan <negociosebansnt@gmail.com>
2024-09-13 12:46:51 -0300

    Varios Controller
    Se agregan los controller de Communications, Customers, Diagnosis

--- MultiserviceApp/src/main/java/com/example/demo/repositories/CustomersRepository.java
+++ MultiserviceApp/src/main/java/com/example/demo/repositories/CustomersRepository.java
@@ -14,7 +14,7 @@ public interface CustomersRepository extends CrudRepository<Customers, Long> {
 
 	List <Customers>findAll();
 	//Esta consulta es para buscar a los clientes por su nombre o apellido (coincidencias)
-	@Query("SELECT c FROM Customers c WHERE LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%')) OR LOWER(c.lastname) LIKE LOWER(CONCAT('%', :name, '%'))")
+	@Query("SELECT c FROM Customers c WHERE LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%')) OR LOWER(c.lastname) LIKE LOWER(CONCAT('%', :name, '%')) OR LIKE CONCAT('%', :name, '%') OR c.alternativePhoneNumber LIKE CONCAT('%', :name, '%')")
 	List<Customers> findByNameContaining(@Param("name") String name);
 	// Esta consulta devuelve a los clientes por su numero de telefono (coincidencias)
 	@Query("SELECT c FROM Customers c WHERE c.phoneNumber LIKE CONCAT('%', :phone, '%') OR c.alternativePhoneNumber LIKE CONCAT('%', :phone, '%')")
